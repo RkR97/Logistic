b5cf968a6ea2148937983ea1800fc029e8fd7acd
RkR97 <rishikeshrai1997@gmail.com>
2021-03-18 10:18:32 +0530

    signin bug fix

--- Project/server/controllers/logisticLoginController.js
+++ Project/server/controllers/logisticLoginController.js
@@ -12,17 +12,18 @@ router.post('', (req, res) => {
         if (err) { throw err; }
         else if (!docs) {
             res.json({ message: "No such user exist", status: 401 });
+        } else {
+            bcrypt.compare(password, docs.password, (err, ans) => {
+                if (err) { throw err }
+                else if (ans) {
+                    let payload = { subject: docs._id }
+                    let token = jwt.sign(payload, 'secretKey')
+                    res.json({ message: "Login Succesfull", status: 200, user: docs, token });
+                } else {
+                    res.json({ message: "password not match", status: 401 })
+                }
+            })
         }
-        bcrypt.compare(password, docs.password, (err, ans) => {
-            if (err) { throw err }
-            else if (ans) {
-                let payload = { subject: docs._id }
-                let token = jwt.sign(payload, 'secretKey')
-                res.json({ message: "Login Succesfull", status: 200, user: docs, token });
-            } else {
-                res.json({ message: "password not match", status: 401 })
-            }
-        })
     })
 })
 
