2af0a01015406ca7f7d2720213a20a49b9a2d4cc
Raviranjan-singh26011998 <64588764+Raviranjan-singh26011998@users.noreply.github.com>
2021-03-20 11:09:16 +0530

    rsponsive added

--- Project/logistic/src/app/signin/signin.component.spec.ts
+++ Project/logistic/src/app/signin/signin.component.spec.ts
@@ -1,25 +1,50 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture} from '@angular/core/testing';
 import { SigninComponent } from './signin.component';
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientModule } from '@angular/common/http';
 
+
+
+/*describe('SigninComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule 
+      ],
+      declarations: [
+        
+      ],
+    }).compileComponents();
+  }));
+});
+*/
 describe('SigninComponent', () => {
   let component: SigninComponent;
   let fixture: ComponentFixture<SigninComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SigninComponent ]
+		
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule 
+      ],
+      declarations: [  ],
     })
     .compileComponents();
   });
 
-  beforeEach(() => {
+ beforeEach(() => {
     fixture = TestBed.createComponent(SigninComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+ it('should create', () => {
     expect(component).toBeTruthy();
   });
+ 
 });
